3f661ab30730c39c66c08eabb2381b4e67277e75
Your Name <you@example.com>
2026-02-09 17:01:41 -0500

    IntakeSubsystem.java created class for SparkMax, set speed for it (no controller code involved yet.) 2026-02-09

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,11 +1,16 @@
-package frc.robot.subsystems;
+package frc.robot.Subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 public class IntakeSubsystem extends SubsystemBase {
 
     public IntakeSubsystem() {
-        
+        Spark mSpark;
+        mSpark = new Spark(0);
+        //figuring out the controller code linked to run the intake
+        mSpark.set(1.5);
+
     }
-    
+
 }
