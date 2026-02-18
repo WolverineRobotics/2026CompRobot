a1f4e8f0f82429a7fc62aebf55b18c399d18ea0c
Knotnu <kweewm@gmail.com>
2026-02-18 16:20:55 -0500

    Started implementing code for intake.

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,18 +1,44 @@
-package frc.robot.Subsystems;
+package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
+
 
 public class IntakeSubsystem extends SubsystemBase {
-        private final Spark mSpark = new Spark(0);
-        private final XboxController iController = new XboxController(0);
-    public IntakeSubsystem() {
+    private final SparkMax rPiviotMotor;
+    private final SparkMax lPiviotMotor;
+    private final SparkMax IntakeMotor;
+    private final DigitalInput LimitAng; //Limit angle
+    private final SparkMaxConfig rPiviotConfig;
+    private final SparkMaxConfig lPiviotConfig;
 
+    public IntakeSubsystem() {
+        rPiviotMotor = new SparkMax(0, MotorType.kBrushless);
+        lPiviotMotor = new SparkMax(1, MotorType.kBrushless);
+        IntakeMotor = new SparkMax(3, MotorType.kBrushless);
+        LimitAng = new DigitalInput(0);
 
         //figuring out the controller code linked to run the intake
     }
+
+
+    public void piviotintake() {
+        /* 
+        get the rPiviotMotor follow the lPiviotMotor down till the DigitalInput says true in the set angle
+*/
+         //where I tried to let the motors move together
+
+
+    }
+
 }
 
 
