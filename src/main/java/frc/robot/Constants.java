240879684800c01c5fc7ce250c3abc5742771cfe
nmuttiah <noah.muttiah@gmail.com>
2026-02-18 12:24:58 -0500

    Began implementing shooter

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public class Constants {
+    public static class ShooterConstants {
+
+        public static final int flywheelCANID = 0; 
+        public static final int indexerCANID = 1; 
+
+        public static final double flyWheelKp = 0; 
+        public static final double flyWheelKi = 0; 
+        public static final double flyWheelKd = 0; 
+
+    }
+}
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,10 +1,51 @@
 package frc.robot.subsystems;
 
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;
 
 public class ShooterSubsystem  extends SubsystemBase {
 
+    private final SparkMax flywheelMotor; 
+    private final SparkMax indexerMotor; 
+
+    private final RelativeEncoder flywheelEncoder; 
+
+    private final PIDController flywheelController; 
+
     public ShooterSubsystem() {
-        
+        flywheelMotor = new SparkMax(
+            ShooterConstants.flywheelCANID, 
+            MotorType.kBrushless
+        ); 
+
+        indexerMotor = new SparkMax(
+            ShooterConstants.indexerCANID, 
+            MotorType.kBrushless
+        ); 
+
+        flywheelEncoder = flywheelMotor.getEncoder(); 
+
+        flywheelController = new PIDController(
+            ShooterConstants.flyWheelKp, 
+            ShooterConstants.flyWheelKi,
+            ShooterConstants.flyWheelKd
+        ); 
     }
+
+    public void setFlyWheelSpeed(double targetSpeed) {
+        flywheelMotor.set(
+            flywheelController.calculate(
+                flywheelEncoder.getVelocity(), 
+                targetSpeed
+            )
+        ); 
+    }
+
+
 }
